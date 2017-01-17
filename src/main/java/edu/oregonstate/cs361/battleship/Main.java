c00294ac58e8ac57ab2bb47313c81f678d85c62d
Michael Hilton <michael.hilton@gmail.com>
2017-01-17 15:49:38 -0800

    initial code commit

---
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -0,0 +1,42 @@
+package edu.oregonstate.cs361.battleship;
+
+import spark.Request;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //This will allow us to server the static pages such as index.html, app.js, etc.
+        staticFiles.location("/public");
+
+        //This will listen to GET requests to /model and return a clean new model
+        get("/model", (req, res) -> newModel());
+        //This will listen to POST requests and expects to receive a game model, as well as location to fire to
+        post("/fire/:row/:col", (req, res) -> fireAt(req));
+        //This will listen to POST requests and expects to receive a game model, as well as location to place the ship
+        post("/placeShip/:id/:row/:col/:orientation", (req, res) -> placeShip(req));
+    }
+
+    //This function should return a new model
+    private static String newModel() {
+        return null;
+    }
+
+    //This function should accept an HTTP request and deseralize it into an actual Java object.
+    private static BattleshipModel getModelFromReq(Request req){
+        return null;
+    }
+
+    //This controller should take a json object from the front end, and place the ship as requested, and then return the object.
+    private static String placeShip(Request req) {
+        return null;
+    }
+
+    //Similar to placeShip, but with firing.
+    private static String fireAt(Request req) {
+        return null;
+    }
+
+}
---
+++ src/main/resources/public/js/app.js
@@ -0,0 +1,104 @@
+var gameModel;
+
+//This function will be called once the page is loaded.  It will get a new game model from the back end, and display it.
+$( document ).ready(function() {
+
+  $.getJSON("model", function( json ) {
+    displayGameState(currModel);
+    gameModel = json;
+   });
+});
+
+function placeShip() {
+   // This ajax call will asnychonously call the back end, and tell it where to place the ship, then get back a game model with the ship placed, and display the new model.
+   var request = $.ajax({
+     url: "/placeShip/"+$( "#shipSelec" ).val()+"/"+$( "#rowSelec" ).val()+"/"+$( "#colSelec" ).val()+"/"+$( "#orientationSelec" ).val(),
+     method: "post",
+     data: JSON.stringify(gameModel),
+     contentType: "application/json; charset=utf-8",
+     dataType: "json"
+   });
+
+   //This will be called when the call is returned from the server.
+   request.done(function( currModel ) {
+     displayGameState(currModel);
+     gameModel = currModel;
+
+   });
+
+   // if there is a problem, and the back end does not respond, then an alert will be shown.
+   request.fail(function( jqXHR, textStatus ) {
+     alert( "Request failed: " + textStatus );
+   });
+}
+
+//Similar to placeShip, but instead it will fire at a location the user selects.
+function fire(){
+   var request = $.ajax({
+     url: "/fire/"+$( "#colFire" ).val()+"/"+$( "#rowFire" ).val(),
+     method: "post",
+     data: JSON.stringify(gameModel),
+     contentType: "application/json; charset=utf-8",
+     dataType: "json"
+   });
+
+   request.done(function( currModel ) {
+     displayGameState(currModel);
+     gameModel = currModel;
+
+   });
+
+   request.fail(function( jqXHR, textStatus ) {
+     alert( "Request failed: " + textStatus );
+   });
+
+}
+
+//This function will display the game model.  It displays the ships on the users board, and then shows where there have been hits and misses on both boards.
+function displayGameState(gameModel){
+$( '#MyBoard td'  ).css("background-color", "blue");
+$( '#TheirBoard td'  ).css("background-color", "blue");
+
+displayShip(gameModel.aircraftCarrier);
+displayShip(gameModel.battleship);
+displayShip(gameModel.cruiser);
+displayShip(gameModel.destroyer);
+displayShip(gameModel.submarine);
+
+for (var i = 0; i < gameModel.computerMisses.length; i++) {
+   $( '#TheirBoard #' + gameModel.computerMisses[i].Across + '_' + gameModel.computerMisses[i].Down ).css("background-color", "green");
+}
+for (var i = 0; i < gameModel.computerHits.length; i++) {
+   $( '#TheirBoard #' + gameModel.computerHits[i].Across + '_' + gameModel.computerHits[i].Down ).css("background-color", "red");
+}
+
+for (var i = 0; i < gameModel.playerMisses.length; i++) {
+   $( '#MyBoard #' + gameModel.playerMisses[i].Across + '_' + gameModel.playerMisses[i].Down ).css("background-color", "green");
+}
+for (var i = 0; i < gameModel.playerHits.length; i++) {
+   $( '#MyBoard #' + gameModel.playerHits[i].Across + '_' + gameModel.playerHits[i].Down ).css("background-color", "red");
+}
+
+
+
+}
+
+
+//This function will display a ship given a ship object in JSON
+function displayShip(ship){
+ startCoordAcross = ship.start.Across;
+ startCoordDown = ship.start.Down;
+ endCoordAcross = ship.end.Across;
+ endCoordDown = ship.end.Down;
+ if(startCoordAcross > 0){
+    if(startCoordAcross == endCoordAcross){
+        for (i = startCoordDown; i <= endCoordDown; i++) {
+            $( '#MyBoard #'+startCoordAcross+'_'+i  ).css("background-color", "yellow");
+        }
+    } else {
+        for (i = startCoordAcross; i <= endCoordAcross; i++) {
+            $( '#MyBoard #'+i+'_'+startCoordDown  ).css("background-color", "yellow");
+        }
+    }
+ }
+}
