09696f192a72846df76a1f7bc8c76d3f91e639b9
Christopher <wohlwenc@oregonstate.edu>
2017-01-31 12:30:11 -0800

    added battleship model class

---
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -0,0 +1,14 @@
+package edu.oregonstate.cs361.battleship;
+
+/**
+ * Created by Christopher on 1/26/2017.
+ */
+public class BattleshipModel {
+
+    private Ship[] playerShips, cpuShips;
+    private Target[] cpuHits, cpuMisses, playerHits, PlayerMisses;
+
+    public BattleshipModel() {
+
+    }
+}
