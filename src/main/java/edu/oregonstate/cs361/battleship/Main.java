1e3e3379fb4e805d84359be92ea494e448e02a0b
trelkins3 <elkinsd@flip1.engr.oregonstate.edu>
2017-03-03 18:50:45 -0800

    Fixed scan.

--- src/main/java/edu/oregonstate/cs361/battleship/Main.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -31,7 +31,7 @@ public class Main {
         return gson.toJson(bm);
     }
 
-    //This function accepts an HTTP request and deseralizes it into an actual Java object.
+    //This function accepts an HTTP request and deserializes it into an actual Java object.
     private static BattleshipModel getModelFromReq(Request req){
         Gson gson = new Gson();
         String result = "";
