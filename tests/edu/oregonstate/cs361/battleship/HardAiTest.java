f3bba5755aba2e7c3caccb718054063482fdd6be
Christopher <wohlwenc@oregonstate.edu>
2017-03-19 22:58:44 -0700

    Fixed tests

--- tests/edu/oregonstate/cs361/battleship/HardAiTest.java
+++ tests/edu/oregonstate/cs361/battleship/HardAiTest.java
@@ -1,16 +1,26 @@
 package edu.oregonstate.cs361.battleship;
 
 import org.junit.jupiter.api.Test;
-/*
+
+import java.util.ArrayList;
+import java.util.List;
+
 class HardAITest {
 
-    @test
-    void fire(List<Coordinate> playerHits , List<Coordinate> playerMisses, Coordinate lastShot){
+    @Test
+    void fire(){
         Coordinate coor = new Coordinate(1,1);
         Coordinate lastShot = new Coordinate(1,1);
+        List<Coordinate> playerHits = new ArrayList<>();
         playerHits.add(0, coor);
-        assertNotNull(fire(playerhits, playermisses, lastShot));
-
+        HardAi hardAI = new HardAi();
+        hardAI.initiateShotQueue();
+        hardAI.fire(playerHits, new ArrayList<>(), lastShot);
+        hardAI.fire(new ArrayList<>(), new ArrayList<>(), lastShot);
+        hardAI.getShipPlacementOf("Computer_AircraftCarrier");
+        hardAI.getShipPlacementOf("Computer_Battleship");
+        hardAI.getShipPlacementOf("Computer_Clipper");
+        hardAI.getShipPlacementOf("Computer_Submarine");
+        hardAI.getShipPlacementOf("Computer_Dinghy");
     }
 }
-*/
